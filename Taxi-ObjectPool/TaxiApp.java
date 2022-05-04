e05e72f350401195ec3bddfb61d38343462c612f
lequoctrang4 <85050886+lequoctrang4@users.noreply.github.com>
2022-05-05 00:26:07 +0700

    First

---
+++ Taxi-ObjectPool/TaxiApp.java
@@ -0,0 +1,13 @@
+public class TaxiApp {
+ 
+    public static final int NUM_OF_CLIENT = 8;
+ 
+    public static void main(String[] args) {
+        TaxiPool taxiPool = new TaxiPool();
+        for (int i = 1; i <= NUM_OF_CLIENT; i++) {
+            Runnable client = new ClientThread(taxiPool);
+            Thread thread = new Thread(client);
+            thread.start();
+        }
+    }
+}
