41bee23d04ba5a69a7c543451e25974a2ac6c539
lequoctrang4 <85050886+lequoctrang4@users.noreply.github.com>
2022-05-05 01:18:12 +0700

    Update File Taxi App

--- Taxi-ObjectPool/TaxiApp.java
+++ Taxi-ObjectPool/TaxiApp.java
@@ -1,12 +1,19 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 public class TaxiApp {
  
-    public static final int NUM_OF_CLIENT = 8;
- 
+    public static final int NUM_OF_CLIENT = 10;
     public static void main(String[] args) {
         TaxiPool taxiPool = new TaxiPool();
         for (int i = 1; i <= NUM_OF_CLIENT; i++) {
             Runnable client = new ClientThread(taxiPool);
             Thread thread = new Thread(client);
+            try {
+                TimeUnit.MILLISECONDS.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             thread.start();
         }
     }
